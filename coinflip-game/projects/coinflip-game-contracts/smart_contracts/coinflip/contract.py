35501d4239d40fef670f04bf1c78bf9560726cb8
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2024-03-31 18:29:00 +0200

    added coinflipstate class

--- coinflip-game/projects/coinflip-game-contracts/smart_contracts/coinflip/contract.py
+++ coinflip-game/projects/coinflip-game-contracts/smart_contracts/coinflip/contract.py
@@ -1,9 +1,54 @@
 import beaker
 import pyteal as pt
 
-app = beaker.Application("coinflip")
+"""
+Author: Tadej Dragojlovic
+Created: 31/03/2024
+"""
 
+class CoinflipState:
+    player_a_choice = beaker.LocalStateValue(
+        stack_type = pt.TealType.bytes,
+        descr = "First player's choice for the coinflip",
+        default = pt.Bytes("Not chosen yet"),
+    )
+
+    player_a_account = beaker.GlobalStateValue(
+        stack_type = pt.TealType.bytes,
+        descr = "First player's account",
+        default = pt.Bytes("No player"),
+    )
+
+    player_b_account = beaker.GlobalStateValue(
+        stack_type = pt.TealType.bytes,
+        descr = "Second player's account",
+        default = pt.Bytes("No player"),
+    )
+
+    wager = beaker.GlobalStateValue(
+        stack_type = pt.TealType.uint64,
+        descr = "Betting amount for the coinflip",
+        default = pt.Int(0),
+    )
+
+app = beaker.Application("coinflip", state=CoinflipState)
 
 @app.external
 def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
     return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+
+@app.create(bare=True)
+def create() -> pt.Expr:
+    """ Method for creating the smart contract, intializing global state """
+
+    return app.initialize_global_state()
+
+@app.opt_in(bare=True)
+def opt_in() -> pt.Expr:
+    """ Optin method """
+
+    return pt.Seq(
+        # TODO: check using conditional statements whether we need to fill player a slot, player b, or if it's full
+
+        app.initialize_local_state(addr=pt.Txn.sender())
+    )
